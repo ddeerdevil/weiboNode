e9573a2a92a477aa7662e9a5def32dfd42fb4b61
sorryzz <80782170@qq.com>
2020-10-21 20:35:53 +0800

    refactor: 调整目录结构

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2020-10-18 09:59:45
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2020-10-18 10:37:15
+ */
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
@@ -6,13 +14,26 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'this is profile page',
+    userName
+  }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+  const { userName, pageIndex } = ctx.params
+  ctx.body = {
+    title: 'loadMore page',
+    userName,
+    pageIndex
   }
 })
 
