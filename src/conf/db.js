ef3454ef8d28239bb98d0510186240f18c067f61
sorryzz <80782170@qq.com>
2020-10-22 22:15:26 +0800

    feat: eslint db seq

--- src/conf/db.js
+++ src/conf/db.js
@@ -4,7 +4,7 @@
  * @Author: sueRimn
  * @Date: 2020-10-22 20:26:44
  * @LastEditors: sueRimn
- * @LastEditTime: 2020-10-22 21:06:31
+ * @LastEditTime: 2020-10-22 22:00:08
  */
 /**
  * @description 存储配置
@@ -18,14 +18,32 @@ let REDIS_CONF = {
     host: '127.0.0.1'
 }
 
+let MYSQL_CONF = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: '3306',
+    database: 'koa2_weibo_db'
+}
+
 if (isProd) {
     REDIS_CONF = {
         // 线上 redis 地址端口
         port: 6379,
         host: '127.0.0.1'
     }
+
+    MYSQL_CONF = {
+        // 线上 mysql
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        port: '3306',
+        database: 'koa2_weibo_db'
+    }
 }
 
 module.exports = {
-    REDIS_CONF
+    REDIS_CONF,
+    MYSQL_CONF
 }
---
+++ src/db/sync.js
@@ -0,0 +1,19 @@
+/**
+ * @description sequelize 同步
+ * @author sorrycc
+ */
+
+const seq = require('./seq');
+
+// require('./model')
+
+seq.authenticate().then(() => {
+    console.log('auth ok');
+}).catch(() => {
+    console.log('auth fail');
+})
+
+seq.sync({ force: true }).then(() => {
+    console.log('sync ok');
+    process.exit();
+});
