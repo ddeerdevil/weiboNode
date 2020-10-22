127bad60d2da1e71fb354d7256fa697cb3405c2e
sorryzz <80782170@qq.com>
2020-10-22 20:51:19 +0800

    feat: redis

---
+++ src/conf/db.js
@@ -0,0 +1,21 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2020-10-22 20:26:44
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2020-10-22 20:28:27
+ */
+/**
+ * @description 存储配置
+ * @author sorryzz
+ */
+
+let REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1'
+}
+
+module.exports = {
+    REDIS_CONF
+}
