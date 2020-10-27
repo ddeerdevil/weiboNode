5a8e56c9a8302a6388813704986302f2afcabc34
sorryzz <80782170@qq.com>
2020-10-27 22:45:00 +0800

    单元

--- test/user/login.test.js
+++ test/user/login.test.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2020-10-27 19:20:23
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2020-10-27 22:44:02
+ */
 const server = require('../server');
 
 // 用户信息
@@ -50,10 +58,40 @@ test('login', async () => {
     COOKIE = res.headers['set-cookie'].join(';');
 })
 
+test('修改基本信息', async () => {
+    const res = await server
+        .patch('/api/user/changeInfo')
+        .send({
+            nickName: '昵称',
+            city: '你的城市',
+            picture: '/test.png'
+        })
+        .set('cookie', COOKIE)
+        expect(res.body.errno).toBe(0)
+})
+
+test('修改密码', async () => {
+    const res = await server
+        .patch('/api/user/changePassword')
+        .send({
+            password,
+            newPassword: `p_${Date.now()}`
+        })
+        .set('cookie', COOKIE)
+        expect(res.body.errno).toBe(0)
+})
+
 test('delete User', async () => {
     const res = await server
         .post('/api/user/delete').set('cookie', COOKIE);
     expect(res.body.errno).toBe(0);
+})
+
+test('退出登录', async () => {
+    const res = await server
+        .post('/api/user/logout')
+        .set('cookie', COOKIE)
+        expect(res.body.errno).toBe(0)
 })
 
 test('再次判断是否存在', async () => {
