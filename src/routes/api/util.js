c2b710eaf561d549785a1d4a1197fe8568e00439
sorryzz <80782170@qq.com>
2020-11-24 11:49:47 +0800

    blog model

--- src/routes/api/util.js
+++ src/routes/api/util.js
@@ -11,6 +11,7 @@ router.prefix('/api/utils')
 
 router.post('/upload', loginCheck, koaFrom(), async (ctx, next) => {
     const file = ctx.req.files['file']
+    if(!file) return
     const { size, path, name, type } = file
     // controller
     ctx.body = await saveFile({
